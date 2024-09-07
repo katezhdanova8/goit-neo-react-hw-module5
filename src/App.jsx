20218dbaeeb660fc589f74ecc169d49ca5cd6739
kate <katezhdanova8@gmail.com>
2024-09-07 21:20:43 +0300

    fix lazy loading

--- src/App.jsx
+++ src/App.jsx
@@ -1,42 +1,47 @@
+import { Suspense, lazy } from 'react';
 import { Routes, Route } from 'react-router-dom';
 import css from './App.module.css';
-import HomePage from './pages/HomePage/HomePage';
-import MoviesPage from './pages/MoviesPage/MoviesPage';
-import MovieDetailsPage from './pages/MovieDetailsPage/MovieDetailsPage';
-import NotFoundPage from './pages/NotFoundPage/NotFoundPage';
 import Navigation from './components/Navigation/Navigation';
 import { Toaster } from 'react-hot-toast';
+import Loader from './components/Loader/Loader';
+
+const HomePage = lazy(() => import('./pages/HomePage/HomePage'));
+const MoviesPage = lazy(() => import('./pages/MoviesPage/MoviesPage'));
+const MovieDetailsPage = lazy(() => import('./pages/MovieDetailsPage/MovieDetailsPage'));
+const NotFoundPage = lazy(() => import('./pages/NotFoundPage/NotFoundPage'));
 
 function App() {
   return (
     <div className={css.App}>
       <Navigation />
-      <Routes>
-        <Route
-          path='/'
-          element={<HomePage />}
-        />
-        <Route
-          path='/movies'
-          element={<MoviesPage />}
-        />
-        <Route
-          path='/movies/:movieId'
-          element={<MovieDetailsPage />}
-        />
-        <Route
-          path='/movies/:movieId/cast'
-          element={<MovieDetailsPage showCast />}
-        />
-        <Route
-          path='/movies/:movieId/reviews'
-          element={<MovieDetailsPage showReviews />}
-        />
-        <Route
-          path='*'
-          element={<NotFoundPage />}
-        />
-      </Routes>
+      <Suspense fallback={<Loader />}>
+        <Routes>
+          <Route
+            path='/'
+            element={<HomePage />}
+          />
+          <Route
+            path='/movies'
+            element={<MoviesPage />}
+          />
+          <Route
+            path='/movies/:movieId'
+            element={<MovieDetailsPage />}
+          />
+          <Route
+            path='/movies/:movieId/cast'
+            element={<MovieDetailsPage showCast />}
+          />
+          <Route
+            path='/movies/:movieId/reviews'
+            element={<MovieDetailsPage showReviews />}
+          />
+          <Route
+            path='*'
+            element={<NotFoundPage />}
+          />
+        </Routes>
+      </Suspense>
       <Toaster />
     </div>
   )
